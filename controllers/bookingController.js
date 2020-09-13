20dc4fe4c7fd592febc323f1a86b525635461f71
Oliver Stuart <olistuart@Oli-Stuarts-macbook-pro.local>
2020-09-13 13:54:41 -0500

    Fixed error in getBookingCheckout function stopping webhooks functioning

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -19,6 +19,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
+        mode: 'payment',
         line_items: [
             {
                 name: `${tour.name} Tour`,
@@ -28,8 +29,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
                         tour.imageCover
                     }`
                 ],
-                amount: tour.price * 100,
-                currency: 'usd',
+                //amount: tour.price * 100,
+                price_data: {
+                    currency: 'usd',
+                    unit_amount: tour.price * 100
+                },
+                //currency: 'usd',
                 quantity: 1
             }
         ]
