ce565a7444460e17b7d287d3aeb65f57918427e7
samir-byte <kiranpoudel28@gmail.com>
2022-01-19 19:52:44 +0545

    query and aggregation middleware implemented

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 const { Schema } = mongoose;
 
 const tourSchema = new Schema({
@@ -7,6 +8,7 @@ const tourSchema = new Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
     },
+    slug: String,
     duration:{
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -47,6 +49,10 @@ const tourSchema = new Schema({
     },
     images: [String],
     startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    },
     createdAt: {
         type: Date,
         default: Date.now(),
@@ -58,6 +64,43 @@ const tourSchema = new Schema({
 
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7
+})
+
+//Document middleware - runs before .save() and .create() not in .insertMany()
+tourSchema.pre('save', function(next){
+    // console.log(this);
+    this.slug = slugify(this.name, {lower: true})
+    next()
+});
+
+// tourSchema.pre('save', function(next){
+//     console.log("pre before saved");
+//     next()
+// });
+
+// tourSchema.post('save', function(doc, next){
+//     console.log(doc);
+//     next()
+// });
+
+//query middleware
+// tourSchema.pre('find', function(next){
+    tourSchema.pre(/^find/, function(next){
+    this.find({secretTour: {$ne: true}})
+    this.start = Date.now()
+    next()
+})
+
+tourSchema.post(/^find/, function(docs, next){
+    console.log(`Query took ${Date.now() - this.start} milliseconds`)
+    // console.log(docs);
+    next()
+})
+
+//aggregate middleware
+tourSchema.pre('aggregate', function(next){
+    this.pipeline().unshift({$match: {secretTour: {$ne: true}}})
+    next()
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
