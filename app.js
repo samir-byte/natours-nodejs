5dd94c91f4b619dfa4027232ad29b56da1d5c9cf
samir-byte <kiranpoudel28@gmail.com>
2022-01-20 13:14:29 +0545

    handled unhandled routes

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use((req, res, next) => {
-    console.log('Hello from middleware')
-    next()
-})
+// app.use((req, res, next) => {
+//     console.log('Hello from middleware')
+//     next()
+// })
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -33,6 +33,13 @@ app.use((req, res, next) => {
 //Routes
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server`
+    })
+})
 
 module.exports = app;
 
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-import validator from 'validator';
+const validator = require('validator')
 const { Schema } = mongoose;
 
 const tourSchema = new Schema({
