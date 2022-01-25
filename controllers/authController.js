ab7e8f4a338e6ca27f7f28298f890ba1c78676fe
samir-byte <kiranpoudel28@gmail.com>
2022-01-25 16:14:53 +0545

    jwt for signup and login implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,15 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+var jwt = require('jsonwebtoken');
+const jwtSecret = process.env.JWT_SECRET;
+var bcrypt = require('bcryptjs');
+
+const signToken = id => {
+    return jwt.sign({id : id}, jwtSecret, {
+        expiresIn: '1h'
+    });
+}
 
 exports.signup = catchAsync(async(req, res, next) => {
     const newUser = await User.create({
@@ -8,11 +18,46 @@ exports.signup = catchAsync(async(req, res, next) => {
         password: req.body.password,
         passwordConfirm: req.body.passwordConfirm
     });
+    const token = signToken(newUser._id)
     res.status(201).json({
         status: 'success',
+        token,
         data: {
             user: newUser
         }
     })
+
+})
+
+exports.login = catchAsync(async(req, res, next) => {
+    // const email = req.body.email;
+    // const password = req.body.password;
+    console.log("this is executing")
+    const {email, password} = req.body
+    //check if email and password exist
+
+    if(!email || !password){
+        return next(new AppError('Please provide email and password', 400));
+    }
+    const user = await User.findOne({email: email}).select('+password');
+    if(!user){
+        return next(new AppError('User doesnot exist', 401));
+    }
+    //check if password is correct
+    if(user){
+        const isMatched = await bcrypt.compare(password, user.password);
+        // console.log(hashedPassword);
+        // console.log(user.password);
+        if(isMatched){
+            const token = signToken(user._id)
+            res.status(200).json({
+                status: 'success',
+                token
+            })
+        }
+        else{
+            return next(new AppError('Password is incorrect', 401));
+        }
+    }
 
 })
