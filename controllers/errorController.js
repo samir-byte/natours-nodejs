e7734dc14243ea1201b03008795a5adb12b4ef3e
samir-byte <kiranpoudel28@gmail.com>
2022-01-25 12:07:02 +0545

    marked mongoose error as operational error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,26 @@
+const AppError = require('./../utils/appError')
+
+const handleValidationErrorDB = err => {
+    console.log(err.message)
+    let message = Object.values(err.errors).map(val => val.message)
+    message = message.join('. ')
+    return new AppError(message, 400)
+}
+
+const handleCastErrorDB = err => {
+    console.log('this is executing')
+    const message = `Invalid ${err.path}: ${err.value}.`
+    console.log(message)
+    return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    console.log(value)
+    const message = `Duplicate field value: ${value}. Please use another value`
+    return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
     return res.status(err.statusCode).json({
         status: err.status,
@@ -34,6 +57,13 @@ module.exports = (err, req, res, next) => {
         sendErrorDev(err, res);
     }
     else if (process.env.NODE_ENV === 'production'){
+        // let error = {...err};
+        // console.log(error.name)
+        // console.log(err)
+        console.log(err.name)
+        if (err.name === 'CastError') err = handleCastErrorDB(err)
+        if (err.code === 11000) err = handleDuplicateFieldsDB(err)
+        if (err.name === 'ValidationError') err = handleValidationErrorDB(err)
         sendErrorProd(err, res);
     }
 }
