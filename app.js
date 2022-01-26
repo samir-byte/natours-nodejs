282a907611086400da47d84a0b17dfb9c62c4d73
samir-byte <kiranpoudel28@gmail.com>
2022-01-26 16:03:50 +0545

    protection middleware data sanitization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -9,11 +12,17 @@ const userRouter = require('./routes/userRoutes')
 
 var app = express()
 
-//Middleware
+//Global Middlewares
+
+//middleware helmet
+app.use(helmet())
+
+//middleware morgan for development
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//rate limiting middleware
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
@@ -25,7 +34,16 @@ const limiter = rateLimit({
 // Apply the rate limiting middleware to all requests
 app.use('/api',limiter);
 
-app.use(express.json());
+//body-parser middleware for req.body
+app.use(express.json({limit: '10kb'}));
+
+//data sanitization middleware noSQL injection
+app.use(mongoSanitize());
+
+//data sanitization middleware against XSS
+app.use(xss());
+
+//serving static files
 app.use(express.static(`${__dirname}/public`))
 
 // app.use((req, res, next) => {
@@ -33,6 +51,7 @@ app.use(express.static(`${__dirname}/public`))
 //     next()
 // })
 
+//test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
@@ -48,6 +67,7 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
 
+//restricting access to routes
 app.all('*', (req, res, next) => {
     // res.status(404).json({
     //     status: 'fail',
@@ -59,6 +79,7 @@ app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server`, 404)) //passing the error to the next middleware this skip all middleware and go to the error handler
 })
 
+//error handler
 app.use(globalErrorHandler)
 
 module.exports = app;
