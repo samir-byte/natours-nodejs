b94a9fcd79806ce5d4b14959a38f4c9c96074145
samir-byte <kiranpoudel28@gmail.com>
2022-02-02 11:44:56 +0545

    adding a _me endpoint to get me user

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,6 +21,11 @@ exports.getAllUsers = factory.getAll(User);
 //     });
 // });
 
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id;
+    next();
+}
+
 exports.updateMe = catchAsync(async(req, res, next) => {
     /*
     1) Create error if user POSTs password data
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,6 +13,8 @@ router.patch('/resetPassword/:token', authController.resetPassword);
 
 router.patch('/updateMyPassword/:id', authController.protect, authController.updatePassword);
 
+router.get('/me', authController.protect, userController.getMe, userController.getUser);
+
 router.patch('/updateMe', authController.protect, userController.updateMe);
 
 router.delete('/deleteMe', authController.protect, userController.deleteMe);
