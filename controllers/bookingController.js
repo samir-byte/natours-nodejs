569511aab2d14a99a77f00a10178fe86005d0c69
samir-byte <kiranpoudel28@gmail.com>
2022-02-09 12:00:15 +0545

    added CRUD routes for bookings

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,6 +4,7 @@ const Tour = require('./../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 const Booking = require('./../models/bookingModel')
+const factory = require('./handlerFactory')
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     /* 
@@ -44,3 +45,9 @@ exports.createBookingCheckout = catchAsync(async(req, res, next) => {
 
     res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBookings = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -4,8 +4,23 @@ const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 const authController = require('../controllers/authController');
 
+router.use(authController.protect);
+
 router
     .post('/checkout-session/:tourId', 
-    authController.protect, bookingController.getCheckoutSession);
+    bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+    .route('/')
+    .get(bookingController.getAllBookings)
+    .post(bookingController.createBooking)
+
+router
+    .route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking)
 
 module.exports = router;
