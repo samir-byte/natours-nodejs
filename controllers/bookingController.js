10a59fba8be665163664d20e95de7a407b3d0322
samir-byte <kiranpoudel28@gmail.com>
2022-02-09 11:51:14 +0545

    created bookings on stripe success temp way more secure way on deployment

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -3,6 +3,7 @@ console.log(process.env.STRIPE_SECRET_KEY)
 const Tour = require('./../models/tourModel')
 const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
+const Booking = require('./../models/bookingModel')
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     /* 
@@ -14,7 +15,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/`,
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
         cancel_url: `${req.protocol}://${req.get('host')}/tours`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -34,5 +35,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         status: 'success',
         session
     })
-
 })
+
+exports.createBookingCheckout = catchAsync(async(req, res, next) => {
+    const { tour, user, price } = req.query;
+    if(!tour && !user && !price) return next();
+    await Booking.create({ tour, user, price })
+
+    res.redirect(req.originalUrl.split('?')[0]);
+});
